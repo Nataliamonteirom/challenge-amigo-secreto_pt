753110d7be706684f8d9ab74a2fbdf3eb52382dc
Natalia Monteiro <monteiron414@gmail.com>
2025-08-04 23:54:19 -0300

    Implementa função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById('amigo');
+    let nome = input.value;
+
+    if (nome === '') {
+        alert('Por favor, insira um nome.');
+        return;
+    }
+
+    amigos.push(nome);
+    input.value = '';
+}
